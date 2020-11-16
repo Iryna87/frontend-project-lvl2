569d41a238aa6c516f0af94d2258a24687e79292
Irena <iryna.morozova87@gmail.com>
2020-11-16 17:23:57 +0100

    update

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,9 @@
 import { test, expect } from '@jest/globals';
 import fs from 'fs';
-import path from 'path';
-import genDiff from '../src/index.js';
+import { dirname, path } from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import genDiff from '../src/index.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
