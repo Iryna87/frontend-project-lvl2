657b5b80b93fa04b20f8711208185e5cf0a40442
Irena <iryna.morozova87@gmail.com>
2020-12-11 21:01:11 +0100

    plaun update

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,20 +10,18 @@ const checkIfValueIsObject = (item) => {
 };
 
 const mapping = {
-  added: (item) => `Property '${item.key}' was added with value: ${checkIfValueIsObject(item.value)}`,
-  removed: (item) => `Property '${item.key}' was removed`,
+  added: (item, name) => `Property '${name}' was added with value: ${checkIfValueIsObject(item.value)}`,
+  removed: (item, name) => `Property '${name}' was removed`,
   unchanged: () => '',
-  changed: (item) => `Property '${item.key}' was updated. From ${checkIfValueIsObject(item.value1)} to ${checkIfValueIsObject(item.value2)}`,
-  nested: (item) => `${((item.children.map((child) => {
-    // eslint-disable-next-line no-param-reassign
-    child.key = `${item.key}.${child.key}`;
-    const result = `${mapping[child.status](child)}`;
+  changed: (item, name) => `Property '${name}' was updated. From ${checkIfValueIsObject(item.value1)} to ${checkIfValueIsObject(item.value2)}`,
+  nested: (item, currentPath) => `${(_.compact(item.children.map((child) => {
+    const result = `${mapping[child.status](child, `${currentPath}.${child.key}`)}`;
     return result;
-  }).filter((e) => String(e).trim())).join('\n'))}`,
+  })).join('\n'))}`,
 };
 
 const plain = (diff) => {
-  const results = diff.map((item) => mapping[item.status](item));
+  const results = diff.map((item) => mapping[item.status](item, item.key));
   return `${results.join('\n')}`;
 };
 
