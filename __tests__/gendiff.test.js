9e1792f78e08c2ca17e37cbd0d2c4bc6c199ddac
Iryna87 <67743648+Iryna87@users.noreply.github.com>
2021-01-08 14:48:25 +0100

    Update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const extentions = ['json', 'yml'];
+
 const getFixturedPath = (pathName) => path.resolve(__dirname, 'fixtures', pathName);
 const readFileContent = (fileName) => fs.readFileSync(getFixturedPath(fileName), 'utf-8');
 
