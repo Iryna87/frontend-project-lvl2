d0113bb4993b633673483a9c6f7c927e8239d11f
Irena <iryna.morozova87@gmail.com>
2021-01-08 13:49:03 +0100

    update gendiff_test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,24 +8,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const extentions = ['json', 'yml'];
-const getResult = (resultName) => fs.readFileSync(path.resolve(__dirname, 'fixtures', `${resultName}.diff`), 'utf8');
-const getFileContent = (fileName, ext) => path.resolve(__dirname, 'fixtures', `${fileName}.${ext}`);
+
+const getFixturePath = (pathName) => fs.readFileSync(path.resolve(__dirname, 'fixtures', pathName, 'utf8'));
+const getFileContent = (fileName, ext) => getFixturePath(`${fileName}.${ext}`);
 
 describe('gendiff', () => {
   describe.each(extentions)('compare two %s files', (ext) => {
     const firstFile = getFileContent('before', ext);
     const secondFile = getFileContent('after', ext);
+    const resultStylish = getFileContent('resultStylish', 'diff');
+    const resultPlain = getFileContent('resultPlain', 'diff');
+    const resultJson = getFileContent('resultJson', 'diff');
 
     test('stylish', () => {
-      expect(genDiff(firstFile, secondFile)).toEqual(getResult('resultStylish'));
+      expect(genDiff(firstFile, secondFile)).toEqual(resultStylish);
     });
 
     test('plain', () => {
-      expect(genDiff(firstFile, secondFile, 'plain')).toEqual(getResult('resultPlain'));
+      expect(genDiff(firstFile, secondFile, 'plain')).toEqual(resultPlain);
     });
 
     test('json', () => {
-      expect(genDiff(firstFile, secondFile, 'json')).toEqual(getResult('resultJson'));
+      expect(genDiff(firstFile, secondFile, 'json')).toEqual(resultJson);
     });
   });
 });
